293d353f973c424216ebb35266fd1b45374cd262
Garrett Blythe <gblythe@aspen-systems.com>
2020-04-03 11:11:20 -0700

    lyric changes

--- index.js
+++ index.js
@@ -7,20 +7,22 @@ axios.get('https://www.lyrics.com/artist/Insane-Clown-Posse/165873')
     if (response.status === 200) {
       const html = response.data;
         const $ = cheerio.load(html);
-        console.log($('.clearfix').find('td').text().trim())
         let albumSong = [
           {
             title: '',
             song: '',
+            url: '',
           }
         ];
         $('.clearfix').each(function(i, elem) {
           const title = $(this).find('h3').text().trim();
           const song = $(this).find('td').text().trim();
+          const url = $(this).find('a').attr(); // fix this, after url is grabbed put it into getLyrics and done
           if (song.length > 0 && i > 0) {
             albumSong[i] = {
               title,
               song,
+              url,
             }
           }
         });
@@ -31,3 +33,15 @@ axios.get('https://www.lyrics.com/artist/Insane-Clown-Posse/165873')
       )
     }
   }, (error) => console.log(error));
+
+const getLyrics = axios.get('https://www.lyrics.com/lyric/32062014/Insane+Clown+Posse/Intro')
+  .then((response) => {
+    if (response.status === 200) {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const lyrics = $('#lyric-body-text').text();
+      fs.writeFile('lyrics.txt', lyrics,
+        (err) => console.log('lyrics done!')
+      )
+    }
+  }, (error) => console.log(error))
